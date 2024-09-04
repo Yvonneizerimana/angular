09c76e0e95d70524caed697a75209977648aac30
Yvonneizerimana <izerimanayvonne510@gmail.com>
2024-09-04 02:17:17 -0700

    data types

--- TypescriptDemo/dataTypes.js
+++ TypescriptDemo/dataTypes.js
@@ -1,4 +1,48 @@
 "use strict";
+//string
 let myNames = "Yvonne IZERIMANA";
 let newMyNames = myNames.toLocaleUpperCase();
 console.log(newMyNames);
+//number
+let age;
+age = 25;
+console.log(age);
+let dob = "25";
+let result = parseInt(dob);
+console.log(result);
+//array
+let empList;
+empList = ["ange", "nelly", "netty", "agabe"];
+console.log(empList);
+let reverse = empList.reverse();
+console.log(reverse);
+let numList;
+numList = [1, 2, 3, 4, 5, 6, 6];
+let filterNumber = numList.filter((num) => num > 3);
+console.log(filterNumber);
+let findNumber = numList.find((num1) => num1 === 2);
+console.log(findNumber);
+let addArray = numList.reduce((a, b) => a + b, 0);
+console.log(addArray);
+//enum
+var color;
+(function (color) {
+    color[color["red"] = 1] = "red";
+    color[color["blue"] = 2] = "blue";
+    color[color["green"] = 3] = "green";
+    color[color["yellow"] = 4] = "yellow";
+})(color || (color = {}));
+let getEnum = color.green;
+console.log(getEnum);
+//tuples
+let swappNumbers;
+let swapp = (num1, num2, num3) => {
+    return [num1, num2, num3];
+};
+console.log(swapp(23, 34, 56));
+//any
+let department;
+department = "IT";
+department = 45;
+department = true;
+console.log(department);
--- TypescriptDemo/dataTypes.ts
+++ TypescriptDemo/dataTypes.ts
@@ -1,3 +1,63 @@
+//string
 let myNames="Yvonne IZERIMANA"
 let newMyNames=myNames.toLocaleUpperCase()
 console.log(newMyNames)
+
+//number
+
+let age:number
+age=25
+console.log(age)
+
+let dob="25"
+let result=parseInt(dob)
+console.log(result)
+
+//array
+
+let empList:string[]
+empList=["ange","nelly","netty","agabe"]
+console.log(empList)
+
+let reverse=empList.reverse()
+console.log(reverse)
+
+let numList:Array<number>
+numList=[1,2,3,4,5,6,6]
+let filterNumber=numList.filter((num)=>num>3)
+console.log(filterNumber)
+
+let findNumber=numList.find((num1)=>num1===2)
+console.log(findNumber)
+
+let addArray=numList.reduce((a,b)=>a+b,0)
+console.log(addArray)
+
+//enum
+
+enum color{
+    red=1,
+    blue,
+    green,
+    yellow
+}
+
+let getEnum:color=color.green
+console.log(getEnum)
+
+//tuples
+
+let swappNumbers:[number,number,number]
+let swapp=(num1:number,num2:number,num3:number):[number,number,number]=>{
+    return [num1,num2,num3]
+}
+console.log(swapp(23,34,56))
+
+//any
+
+let department:any
+department="IT"
+department=45
+department=true
+
+console.log(department)
